5ed7738bebdd92347553e82545e301570f65cda0
vages <eirikvageskar@gmail.com>
2015-03-13 23:52:20 +0100

    Add plurality value function

--- ex04/decision_tree.py
+++ ex04/decision_tree.py
@@ -1,7 +1,26 @@
 __author__ = 'eirikvageskar'
 
 
-def decision_tree_learning(examples, attribute_set, parent_examples):
+def plurality_value(examples, example_numbers):
+    """Finds the most common output value (i.e. last value) among a set of examples.
+
+    :param examples: The complete set of examples.
+    :param example_numbers: The examples to be examined.
+    :return: The most common result.
+    """
+    counts = {}
+
+    for i in example_numbers:
+        result = examples[i][-1]
+        try:
+            counts[result] += 1
+        except KeyError:
+            counts[result] = 1
+
+    return max(counts, key=lambda x: counts[x])
+
+
+def decision_tree_learning(examples, example_numbers, attribute_set, parent_example_numbers):
     """Returns a decision tree.
 
     Builds on fig. 18.5 from Artificial Intelligence: A modern approach.
